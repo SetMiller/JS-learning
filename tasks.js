a795201ac3cd29deb5f0e63ab0c041dbc4767107
setmiller <setmiller@gmail.com>
2019-02-28 22:32:05 +0300

    update

--- tasks.js
+++ tasks.js
@@ -1,31 +1,36 @@
-//     let test1 = [[1,2],[3,4],[5,6]];
-//     let test2 = [[1,3,5],[2,4,6]];
-//     let test3 = [[1,1],[1],[1,1]];
+////////////////////////////////////////
+// Интересные задачи для разбора
+////////////////////////////////////////
 
-//     function bigToSmall(arr){
-//       var result=[].concat(...arr).sort().reverse().join(">");
-//       console.log(result);
-//       }
-
-
-/////////////////////////////////////////////////////////////////////////////////
+// 1️⃣
+// The two arrays are not empty and are the same length. 
+// Return the score for this array of answers, giving +4 for each correct answer, 
+// -1 for each incorrect answer, 
+// and +0 for each blank answer(empty string).
+// If the score < 0, return 0.
+// checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+// checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+// checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+// checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
 let array1 = ["b", "c", "b", "a"]; 
 let array2 = ["",  "a", "a", "c"];
-
+// вариант ECMAScript 3
 // function checkExam(array1, array2) {
 //   let score = 0;
-//   for (i = array1.length; i--;) {
-//     if (array1[i] == array2[i]) score += 4;
-//     else if  (array1[i] !== array2[i] && array2[i]) score -= 1;
-//     else if (score < 0) score = 0;
-//     console.log(`i = ${i} - score = ${score}`)
-//   }
-//   return score;
+//    for (i = array1.length; i--;) {
+//      if (array1[i] == array2[i]) score += 4;
+//      else if (array2[i]) score -= 1;
+//    }
+//    return score < 0 ? 0 : score;
 //  }
 
+// вариант ECMAScript 5
 checkExam=(arr1,arr2)=>
-   Math.max( 
-    arr2.reduce( (a,b,i) => b==arr1[i] ? a+4: b ? a-1: a ,0) 
-    ,0)
+  Math.max(                                                         //👈 Если результат меньше 0, то возвращаем 0
+    arr2.reduce(                                                    //👈 Метод reduce(), как замена циклу for с аккумулятором значений
+      (a,b,i) => b==arr1[i] ? a+4 :                                 //👈 Если текущие элементы массива равны, то добавляем 4 балл
+                 b ? a-1 :                                          //👈 Если текущий элемент массива не empty (null, undefined), то отнимает 1 балл
+                 a                                                  //👈 Если текущий элемент массива empty, то не делаем ничего (0 баллов)
+    ,0)                                                             //👈 Начальное значение счетчика
+  ,0)
 
-console.log(checkExam(array1, array2));
